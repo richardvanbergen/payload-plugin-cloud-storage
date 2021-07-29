ba6cceaf657e69f06efb2b58ca04f6d47ac4b6d0
Richard Vanbergen <richard.vanbergen@gmail.com>
2021-07-29 18:52:05 +0900

    feat: adding basic tests

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -1,5 +1,73 @@
-import plugin from './plugin'
+import { AdapterInterface } from './payload-plugin-s3'
+import { deleteHook, uploadHook } from './plugin'
+import { mock as mockInterface } from 'jest-mock-extended'
+import { UploadedFile } from 'express-fileupload'
 
 describe('main plugin', () => {
-  it.todo('write a test')
+  let adapter: AdapterInterface
+  const testFile = mockInterface<UploadedFile>({
+    name: 'test.image'
+  })
+
+  beforeEach(() => {
+    adapter = mockInterface<AdapterInterface>()
+  })
+
+  it('can make a hook that calls adapters delete method', () => {
+    const initializedHook = deleteHook(adapter)
+    // @ts-expect-error don't need to mock out entire request
+    initializedHook({ doc: { filename: 'test.jpg' } })
+    expect(adapter.delete).toBeCalledTimes(1)
+  })
+
+  it('can make a hook that calls adapters upload method', () => {
+    const initializedHook = uploadHook(adapter)
+    const data = {
+      filename: 'filename.image'
+    }
+
+    const req = {
+      files: {
+        file: testFile
+      }
+    }
+
+    initializedHook({
+      data,
+      // @ts-expect-error don't need to mock out entire request
+      req
+    })
+
+    expect(adapter.upload).toBeCalledWith('filename.image', testFile)
+    expect(adapter.upload).toBeCalledTimes(1)
+  })
+
+  it('can fetch first file from array of files', () => {
+    const initializedHook = uploadHook(adapter)
+    const data = {
+      filename: 'filename.image'
+    }
+
+    const testFile2 = mockInterface<UploadedFile>({
+      name: 'test.image'
+    })
+
+    const req = {
+      files: {
+        file: [
+          testFile2,
+          testFile,
+        ]
+      }
+    }
+
+    initializedHook({
+      data,
+      // @ts-expect-error don't need to mock out entire request
+      req
+    })
+
+    expect(adapter.upload).toBeCalledWith('filename.image', testFile2)
+    expect(adapter.upload).toBeCalledTimes(1)
+  })
 })
