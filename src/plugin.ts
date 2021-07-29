8813dd3bbedbe17758a21d8ba25812f2f5abe133
Richard Vanbergen <richard.vanbergen@gmail.com>
2021-07-29 16:44:00 +0900

    WIP: removing weird adapter initialisation in plugin to make it more easily testable

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,50 +1,45 @@
 import { SanitizedConfig } from 'payload/config'
 import { UploadedFile } from 'express-fileupload'
 import { CollectionBeforeChangeHook, CollectionAfterDeleteHook } from 'payload/types'
-// @ts-ignore
-import { APIError } from 'payload/errors'
 import { AdapterInterface, S3PluginCollectionModifiers } from './payload-plugin-s3'
 
-let adapterInstance: AdapterInterface
-const getAdapter = (): AdapterInterface => {
-  if (!adapterInstance) {
-    throw new APIError("Adapter has not been initialized.")
-  }
-  return adapterInstance
-}
+export const uploadHook = (adapter: AdapterInterface) => {
+  const beforeChange: CollectionBeforeChangeHook = async (args) => {
+    if (args) {
+      const { req, data } = args
+      if (req?.files?.file) {
+        let uploadedFile: UploadedFile
+        if (Array.isArray(req.files.file)) {
+          uploadedFile = req.files.file[0]
+        } else {
+          uploadedFile = req.files.file
+        }
 
-export const uploadHook: CollectionBeforeChangeHook = async (args) => {
-  if (args) {
-    const { req, data } = args
-    if (req?.files?.file) {
-      let uploadedFile: UploadedFile
-      if (Array.isArray(req.files.file)) {
-        uploadedFile = req.files.file[0]
-      } else {
-        uploadedFile = req.files.file
+        await adapter.upload(data.filename, uploadedFile)
       }
 
-      const adapter = getAdapter()
-      await adapter.upload(data.filename, uploadedFile)
+      return data
     }
-
-    return data
   }
+
+  return beforeChange
 }
 
-export const deleteHook: CollectionAfterDeleteHook = async (args) => {
-  if (args) {
-    const { doc } = args
-    const adapter = getAdapter()
-    await adapter.delete(doc.filename)
+export const deleteHook = (adapter: AdapterInterface) => {
+  const afterDelete: CollectionAfterDeleteHook = async (args) => {
+    if (args) {
+      const { doc } = args
+      await adapter.delete(doc.filename)
+    }
   }
+
+  return afterDelete
 }
 
 const cloudStorage = (
   adapter: AdapterInterface,
   uploadCollectionModifiers?: S3PluginCollectionModifiers
 ) => {
-  adapterInstance = adapter
   return (incommingConfig: SanitizedConfig): SanitizedConfig => {
     const config: SanitizedConfig = {
       ...incommingConfig,
@@ -66,11 +61,11 @@ const cloudStorage = (
             ...collection.hooks,
             beforeChange: [
               ...beforeChange,
-              uploadHook,
+              uploadHook(adapter),
             ],
             afterDelete: [
               ...afterDelete,
-              deleteHook,
+              deleteHook(adapter),
             ],
           }
 
